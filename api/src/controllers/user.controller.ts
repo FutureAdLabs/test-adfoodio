cb75d61c59571a22bc28a8eaede872fce611d7ae
Alicia Rojo <72359305+aliciarojovera@users.noreply.github.com>
2021-02-10 00:22:02 +0100

    Update user.controller.ts

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -15,15 +15,12 @@ export async function newUser(req: Request, res: Response) {
 
 // Function to collect data from old user orders 
 export async function getUser(req: Request, res: Response) {
-  console.log('=================================> 1')
   try {
     const conn = await connect();
-    console.log('=================================> 2')
     const user: any = await conn.query("SELECT * FROM users WHERE email = ? ", [
       req.body.email
     ]);
     conn.end();
-    console.log('=================================> 3')
     let orders = user[0][0].orders.id;
     // console.log('orders', orders)
     // console.log('orders LAST', orders[orders.length -1], orders[orders.length -2], orders[orders.length -3])
@@ -36,20 +33,15 @@ export async function getUser(req: Request, res: Response) {
     } 
 
     let arrayRes: any = [];
-    console.log('=================================> 4')
     await Promise.all(
       orders.map(async (order: any) => {
-        console.log('=================================> 5 MAP')
         const conn = await connect();
         const userOrder: any = await conn.query(
           `SELECT * FROM orders WHERE JSON_CONTAINS(sentence, ?)`,
           `{"id": ${order.toString()}}`
         );
-        console.log('=================================> 6')
         conn.end();
-        console.log('=================================> 7')
         if (userOrder[0][0]) {
-          console.log('=================================> 8')
           arrayRes.push(userOrder[0][0].sentence);
         } else {
           // For avoiding errors reading de Database
